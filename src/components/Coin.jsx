1f7627c03e239172f6ee5b9529eec274143225fc
geoffreykithuku <mutembeikithuku12@gmail.com>
2022-10-04 23:27:26 +0300

    added style to featured component

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./css/Featured.css";
 import { FiArrowUpRight, FiArrowDown } from "react-icons/fi";
 
 const Coin = ({ image, coinname, price, percentage }) => {
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -4,7 +4,7 @@ import Coin from "./Coin";
 import axios from "axios";
 const Featured = () => {
   const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=9&page=1&sparkline=false";
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false";
 
   const [data, setData] = useState([]);
 
